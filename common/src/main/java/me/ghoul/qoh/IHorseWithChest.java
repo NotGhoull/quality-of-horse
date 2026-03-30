096abd94d456de92ff42112f5188a7a1d35f42b1
NotGhoull <NotGhoulDev@proton.me>
2026-03-30 22:28:31 +0100

    feat: Add chest rendering for the horse
    fix: Fabric linking issues
    
    Co-authored-by: LieOnLion <lion@lieonlion.dev>

---
+++ common/src/main/java/me/ghoul/qoh/IHorseWithChest.java
@@ -0,0 +1,5 @@
+package me.ghoul.qoh;
+
+public interface IHorseWithChest {
+    public boolean qoh$hasChest();
+}
---
+++ common/src/main/java/me/ghoul/qoh/mixin/HorseRendererMixin.java
@@ -0,0 +1,24 @@
+package me.ghoul.qoh.mixin;
+
+import me.ghoul.qoh.client.renderer.HorseChestLayer;
+import net.minecraft.client.model.HorseModel;
+import net.minecraft.client.renderer.entity.*;
+import net.minecraft.world.entity.animal.horse.Horse;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(HorseRenderer.class)
+public abstract class HorseRendererMixin extends AbstractHorseRenderer<Horse, HorseModel<Horse>> {
+    public HorseRendererMixin(EntityRendererProvider.Context pContext, HorseModel<Horse> pModel, float pScale) {
+        super(pContext, pModel, pScale);
+    }
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void addChestLayer(CallbackInfo ci) {
+        this.addLayer(new HorseChestLayer(this));
+    }
+}
+
+
