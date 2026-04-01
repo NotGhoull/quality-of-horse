74ededa10f7c52856f0378e5d9c86d41732a1cb4
NotGhoull <NotGhoulDev@proton.me>
2026-04-01 16:03:23 +0100

    feat: Move to architectury

--- common/src/main/java/me/ghoul/qoh/CommonClass.java
+++ common/src/main/java/me/ghoul/qoh/CommonClass.java
@@ -1,12 +1,13 @@
 package me.ghoul.qoh;
 
-import me.ghoul.qoh.platform.Services;
+//import me.ghoul.qoh.platform.Services;
 
 public class CommonClass {
 
     public static void init() {
-        if (Services.PLATFORM.isModLoaded("qoh")) {
-            Constants.LOG.info("Peter the horse is here");
-        }
+//        if (Services.PLATFORM.isModLoaded("qoh")) {
+//            Constants.LOG.info("Peter the horse is here");
+//      }
+        Constants.LOG.info("Peter the horse is here");
     }
 }
