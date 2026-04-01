3e2bdacd575e3eab0b6eee83de84fd6fff97d190
NotGhoull <NotGhoulDev@proton.me>
2026-04-01 22:27:25 +0100

    fix(common): right chest being slightly more inset than the left
    Co-authored-by: LieOnLion <lion@lieonlion.dev>

--- common/src/main/java/me/ghoul/qoh/client/renderer/HorseChestLayer.java
+++ common/src/main/java/me/ghoul/qoh/client/renderer/HorseChestLayer.java
@@ -32,7 +32,7 @@ public class HorseChestLayer<T extends LivingEntity, M extends EntityModel<T>> e
         CubeListBuilder cubeListBuilder = CubeListBuilder.create()
                 .texOffs(0, 0)
                 .addBox(-4, 0, -2, 8, 8, 3);
-        partDefinition.addOrReplaceChild("right_chest", cubeListBuilder, PartPose.offsetAndRotation(-6, 3, 5, 0, -HALF_PI, 0));
+        partDefinition.addOrReplaceChild("right_chest", cubeListBuilder, PartPose.offsetAndRotation(-7, 3, 5, 0, -HALF_PI, 0));
         partDefinition.addOrReplaceChild("left_chest", cubeListBuilder, PartPose.offsetAndRotation(6, 3, 5, 0, -HALF_PI, 0));
 
         this.chestModel = partDefinition.bake(22, 11);
