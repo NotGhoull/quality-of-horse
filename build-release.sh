296d67d1834ab3f6a2dfc35d6a113bb59467b6e5
NotGhoull <NotGhoulDev@proton.me>
2026-04-04 23:02:39 +0100

    feat: release 1.1.0

--- build-release.sh
+++ build-release.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 set -e
 
-echo "Building mod JARs..."
+MOD_VERSION=$(grep "^mod_version=" gradle.properties | cut -d'=' -f2)
+MINECRAFT_VERSION=$(grep "^minecraft_version=" gradle.properties | cut -d'=' -f2)
 
-# Build with Java 21
+echo "Building mod JARs (mod_version=$MOD_VERSION, minecraft_version=$MINECRAFT_VERSION)..."
+
 export JAVA_HOME=/usr/lib/jvm/java-21-openjdk
 ./gradlew build
 
-# Create release directory
 mkdir -p release
 
-# Copy the distribution JARs to release folder
-cp fabric/build/libs/qoh-fabric-*.jar release/
-#cp forge/build/libs/"Quality of horse-forge-1.21-"*.jar release/
-cp neoforge/build/libs/qoh-neoforge-*.jar release/
+cp fabric/build/libs/qoh-fabric-${MOD_VERSION}+${MINECRAFT_VERSION}-${MOD_VERSION}.jar release/
+mv release/qoh-fabric-${MOD_VERSION}+${MINECRAFT_VERSION}-${MOD_VERSION}.jar release/qoh-fabric-${MOD_VERSION}+${MINECRAFT_VERSION}.jar
+
+cp neoforge/build/libs/qoh-neoforge-${MOD_VERSION}+${MINECRAFT_VERSION}-${MOD_VERSION}.jar release/
+mv release/qoh-neoforge-${MOD_VERSION}+${MINECRAFT_VERSION}-${MOD_VERSION}.jar release/qoh-neoforge-${MOD_VERSION}+${MINECRAFT_VERSION}.jar
 
 echo "Release files created in ./release:"
 ls -la release/
